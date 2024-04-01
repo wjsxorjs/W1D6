4b829a289cb9af45b9dd649d8c12ccd859a94b5e
wjsxorjs <wjsxorjs@naver.com>
2024-04-01 10:11:35 +0900

    JAVA 2024.04.01 0.1

---
+++ Ex20240401/src/am/CarEx1.java
@@ -0,0 +1,24 @@
+package am;
+
+public class CarEx1 {
+	
+	String model;
+	int price;
+	String maker;
+	
+	public void setModel(String newModel) {
+		//인자를 받아서 model이라는 멤버변수에 저장한다.
+		model = newModel;
+	}
+
+	public void setPrice(int newPrice) {
+		//인자를 받아서 price이라는 멤버변수에 저장한다.
+		price = newPrice;
+	}
+	
+	public void setMaker(String newMaker) {
+		//인자를 받아서 maker이라는 멤버변수에 저장한다.
+		maker = newMaker;
+	}
+
+}
