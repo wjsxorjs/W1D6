957b63b11e3645d87ed46e25abe4cc4bdb77c208
wjsxorjs <wjsxorjs@naver.com>
2024-04-01 12:26:20 +0900

    JAVA 2024.04.01 0.31

--- Ex20240401/src/am/MainEx3.java
+++ Ex20240401/src/am/MainEx3.java
@@ -17,13 +17,19 @@ public class MainEx3 {
 		ParentEx3 pa3 = new GrandChildEx3();
 		
 		test(pa1);
+		System.out.println("ParentEx3 pa1 = new ParentEx3();");
 		System.out.println(pa1.msg);
+		System.out.println();
 		
 		test(pa2);
+		System.out.println("ParentEx3 pa1 = new ChildEx3();");
 		System.out.println(pa2.msg);
+		System.out.println();
 		
 		test(pa3);
+		System.out.println("ParentEx3 pa1 = new GrandChildEx3();");
 		System.out.println(pa3.msg);
+		System.out.println();
 		
 		
 		
